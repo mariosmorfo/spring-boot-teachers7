04517ec7c799dccf17f9936dbd7d863b9f8b9d65
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-29 19:03:19 +0300

    feat: Custom exceptions created

---
+++ src/main/java/gr/aueb/cf/teacherapp/core/exceptions/AppServerException.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.teacherapp.core.exceptions;
+
+public class AppServerException extends EntityGenericException {
+
+
+    public AppServerException(String code, String message){
+        super(code , message);
+
+    }
+
+}
