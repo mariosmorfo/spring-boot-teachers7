b97a2e7a0edc0a7adedd9f02cdf30a7140093d40
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-30 16:35:08 +0300

    feat; create region repository

--- src/main/java/gr/aueb/cf/teacherapp/repository/RegionRepository.java
+++ src/main/java/gr/aueb/cf/teacherapp/repository/RegionRepository.java
@@ -1,8 +1,9 @@
 package gr.aueb.cf.teacherapp.repository;
 
 import gr.aueb.cf.teacherapp.model.static_data.Region;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RegionRepository extends JpaSpecificationExecutor<Region> {
+public interface RegionRepository extends JpaRepository<Region, Long>, JpaSpecificationExecutor<Region>{
 }
