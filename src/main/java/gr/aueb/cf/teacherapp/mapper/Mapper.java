3f5991bdecb6d459832d69f9d1cec18fd0ea8996
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-01 14:41:32 +0300

    feat: Mapper created for User and Teacher model

--- src/main/java/gr/aueb/cf/teacherapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/teacherapp/mapper/Mapper.java
@@ -1,9 +1,14 @@
 package gr.aueb.cf.teacherapp.mapper;
 
+import gr.aueb.cf.teacherapp.core.enums.Role;
 import gr.aueb.cf.teacherapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.teacherapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.teacherapp.dto.UserInsertDTO;
 import gr.aueb.cf.teacherapp.model.Teacher;
+import gr.aueb.cf.teacherapp.model.User;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class Mapper {
@@ -20,5 +25,9 @@ public class Mapper {
     public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher){
         return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(), teacher.getUpdatedAt(), teacher.getUuid(), teacher.getFirstname(),
                 teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
+    }
+
+    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
+        return new User(null, userInsertDTO.getUsername(), userInsertDTO.getPassword(), Role.valueOf(userInsertDTO.getRole().toUpperCase()));
     }
 }
